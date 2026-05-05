9e50cfeabb6b6892d90c9d99bfe325627f4e0197
skr2rathishan-oss <skr2rathishan@gmail.com>
2026-05-06 00:06:28 +0530

    Refactor data structure headers to use include guards and implement basic functionality for Graph, HashTable, LinkedList, Queue, Stack, and Tree classes.

--- backend/data_structures/Graph.h
+++ backend/data_structures/Graph.h
@@ -1,1 +1,74 @@
-#pragma once
+#ifndef GRAPH_H
+#define GRAPH_H
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Graph {
+private:
+    static const int MAX = 20;
+    string nodes[MAX];
+    int adj[MAX][MAX];
+    int count;
+
+public:
+    Graph() {
+        count = 0;
+        for (int i = 0; i < MAX; i++) {
+            for (int j = 0; j < MAX; j++) {
+                adj[i][j] = 0;
+            }
+        }
+    }
+
+    void addNode(string name) {
+        if (count < MAX) {
+            nodes[count] = name;
+            count++;
+        }
+    }
+
+    int findNode(string name) {
+        for (int i = 0; i < count; i++) {
+            if (nodes[i] == name) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    void addEdge(string from, string to) {
+        int i = findNode(from);
+        int j = findNode(to);
+
+        if (i != -1 && j != -1) {
+            adj[i][j] = 1;
+        }
+    }
+
+    bool hasEdge(string from, string to) {
+        int i = findNode(from);
+        int j = findNode(to);
+
+        if (i != -1 && j != -1) {
+            return adj[i][j] == 1;
+        }
+        return false;
+    }
+
+    void display() {
+        cout << "\nGraph Connections:\n";
+        for (int i = 0; i < count; i++) {
+            cout << nodes[i] << " -> ";
+            for (int j = 0; j < count; j++) {
+                if (adj[i][j] == 1) {
+                    cout << nodes[j] << " ";
+                }
+            }
+            cout << endl;
+        }
+    }
+};
+
+#endif
--- backend/data_structures/LinkedList.h
+++ backend/data_structures/LinkedList.h
@@ -1,1 +1,61 @@
-#pragma once
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+
+#include <iostream>
+using namespace std;
+
+template <class T>
+class LinkedList {
+private:
+    struct Node {
+        T data;
+        Node* next;
+
+        Node(T value) {
+            data = value;
+            next = nullptr;
+        }
+    };
+
+    Node* head;
+
+public:
+    LinkedList() {
+        head = nullptr;
+    }
+
+    void insertAtEnd(T value) {
+        Node* newNode = new Node(value);
+
+        if (head == nullptr) {
+            head = newNode;
+            return;
+        }
+
+        Node* temp = head;
+        while (temp->next != nullptr) {
+            temp = temp->next;
+        }
+
+        temp->next = newNode;
+    }
+
+    bool isEmpty() {
+        return head == nullptr;
+    }
+
+    Node* getHead() {
+        return head;
+    }
+
+    void display() {
+        Node* temp = head;
+        while (temp != nullptr) {
+            cout << temp->data << " ";
+            temp = temp->next;
+        }
+        cout << endl;
+    }
+};
+
+#endif
